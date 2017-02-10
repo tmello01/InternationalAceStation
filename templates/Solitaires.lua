3cca8937a7ade0f2a114a72b52a2f838eabd3c32
ebernerd <micromacroeric@gmail.com>
2017-02-09 21:20:30 -0500

    Fixed solitaire crash

---
+++ templates/Solitaires.lua
@@ -0,0 +1,516 @@
+local t = 
+{
+	["cards"] = 
+	{
+		[10] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 690,
+			["y"] = 255,
+		},
+		[11] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 776,
+			["y"] = 206,
+		},
+		[12] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 776,
+			["y"] = 223,
+		},
+		[13] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 776,
+			["y"] = 239,
+		},
+		[14] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 776,
+			["y"] = 256,
+		},
+		[15] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 207,
+		},
+		[16] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 223,
+		},
+		[17] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 237,
+		},
+		[18] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 254,
+		},
+		[19] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 776,
+			["y"] = 272,
+		},
+		[1] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 524,
+			["y"] = 206,
+		},
+		[20] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 272,
+		},
+		[21] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 860,
+			["y"] = 286,
+		},
+		[22] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 207,
+		},
+		[23] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 222,
+		},
+		[24] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 236,
+		},
+		[25] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 252,
+		},
+		[26] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 270,
+		},
+		[27] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 284,
+		},
+		[28] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 944,
+			["y"] = 300,
+		},
+		[2] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 441,
+			["y"] = 206,
+		},
+		[3] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 524,
+			["y"] = 221,
+		},
+		[4] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 605,
+			["y"] = 206,
+		},
+		[5] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 605,
+			["y"] = 223,
+		},
+		[6] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = false,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 605,
+			["y"] = 239,
+		},
+		[7] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 690,
+			["y"] = 206,
+		},
+		[8] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 690,
+			["y"] = 223,
+		},
+		[9] = 
+		{
+			["deckgroup"] = 1486321725,
+			["flipped"] = true,
+			["suit"] = "any",
+			["value"] = "any",
+			["x"] = 690,
+			["y"] = 240,
+		},
+	},
+	["deckgroups"] = 
+	{
+		[1] = 
+		{
+			["allowrepeats"] = false,
+			["cards"] = 52,
+			["id"] = 1486321725,
+			["preset"] = 
+			{
+				["clubs"] = 
+				{
+					[10] = "j",
+					[11] = "q",
+					[12] = "k",
+					[13] = "a",
+					[1] = "2",
+					[2] = "3",
+					[3] = "4",
+					[4] = "5",
+					[5] = "6",
+					[6] = "7",
+					[7] = "8",
+					[8] = "9",
+					[9] = "10",
+				},
+				["diamonds"] = 
+				{
+					[10] = "j",
+					[11] = "q",
+					[12] = "k",
+					[13] = "a",
+					[1] = "2",
+					[2] = "3",
+					[3] = "4",
+					[4] = "5",
+					[5] = "6",
+					[6] = "7",
+					[7] = "8",
+					[8] = "9",
+					[9] = "10",
+				},
+				["hearts"] = 
+				{
+					[10] = "j",
+					[11] = "q",
+					[12] = "k",
+					[13] = "a",
+					[1] = "2",
+					[2] = "3",
+					[3] = "4",
+					[4] = "5",
+					[5] = "6",
+					[6] = "7",
+					[7] = "8",
+					[8] = "9",
+					[9] = "10",
+				},
+				["spades"] = 
+				{
+					[10] = "j",
+					[11] = "q",
+					[12] = "k",
+					[13] = "a",
+					[1] = "2",
+					[2] = "3",
+					[3] = "4",
+					[4] = "5",
+					[5] = "6",
+					[6] = "7",
+					[7] = "8",
+					[8] = "9",
+					[9] = "10",
+				},
+			},
+		},
+	},
+	["decks"] = 
+	{
+		[1] = 
+		{
+			["cards"] = 
+			{
+				[10] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[11] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[12] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[13] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[14] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[15] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[16] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[17] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[18] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[19] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[1] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[20] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[21] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[22] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[23] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[24] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[2] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[3] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[4] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[5] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[6] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[7] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[8] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+				[9] = 
+				{
+					["deckgroup"] = 1486321725,
+					["flipped"] = true,
+					["suit"] = "any",
+					["value"] = "any",
+				},
+			},
+			["deckgroup"] = 1486321725,
+			["x"] = 159,
+			["y"] = 205,
+		},
+	},
+}
+
+return t
